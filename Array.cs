a7e10e6495d734fa813b0dfa3db97937c017219f
Jyothirmaidumpala <98098605+Jyothirmaidumpala@users.noreply.github.com>
2022-03-03 08:02:29 +0530

    Assignment1
    Assignmnent1

---
+++ Array.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Array
+    {
+        static void Main()
+        {
+            int[] arr = { 50, 10, 20, 30, 40 };
+            int sum = 0;
+            int max, min;
+            float avg = 0.0F;
+            sum = arr.Sum();           
+            avg = sum / arr.Length;
+            Console.WriteLine($"The average value of array is {avg}");
+
+            max = arr[0];
+            min = arr[1];
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] > max)
+                {
+                    max = arr[i];
+                }
+                if (arr[i] < min)
+                {
+                    min = arr[i];
+                }
+            }
+            Console.WriteLine("The minimum value of array " + min);
+            Console.WriteLine("The maximum value of array " + max);
+        }
+    }
+}
+
---
+++ Marks.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment1
+{
+    class Marks
+    {
+        static void Main()
+        {
+            string name;
+            int n = 3;
+            float sum, avg, Maths, Science, Social;
+
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("Enter Student Name:");
+                name = Console.ReadLine();
+                Console.WriteLine("Enter Maths Marks:");
+                Maths = Convert.ToSingle(Console.ReadLine());
+                Console.WriteLine("Enter Science Marks:");
+                Science = Convert.ToSingle(Console.ReadLine());
+                Console.WriteLine("Enter Social Marks:");
+                Social = Convert.ToSingle(Console.ReadLine());
+                sum = Maths + Science + Social;
+                avg = sum / n;
+                if (avg > 50)
+                {
+                    Console.WriteLine("Passed");
+                }
+                else
+                {
+                    Console.WriteLine("Failed");
+                }
+            }
+        }
+    }
+}
