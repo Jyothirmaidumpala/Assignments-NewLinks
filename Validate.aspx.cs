1f83a99bffe1d7bed224e35e218d0e7098006ee0
Jyothirmaidumpala <98098605+Jyothirmaidumpala@users.noreply.github.com>
2022-03-13 21:00:11 +0530

    Asp_Assignment1

---
+++ Validate.aspx.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Asp_Assignment1
+{
+    public partial class Validate : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                Response.Redirect("Valid.html");
+            }
+            else
+            {
+                Response.Redirect("Validation Failed......!");
+            }
+        }
+    }
+}
