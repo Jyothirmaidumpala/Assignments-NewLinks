bc7cecdc0fba6cafc3cb86542ea6e3ead0e8e070
Jyothirmaidumpala <98098605+Jyothirmaidumpala@users.noreply.github.com>
2022-03-13 21:51:52 +0530

    Asp_DropDown Assignment

---
+++ DropDownlist.aspx
@@ -0,0 +1,20 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="DropDownlist.aspx.cs" Inherits="Asp_DropDown.DropDownlist" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+        Select Any Item <asp:DropDownList ID="DropDownList1" runat="server" Height="50px" Width="50px" AutoPostBack="True" OnSelectedIndexChanged="DropDownList1_SelectedIndexChanged">
+        </asp:DropDownList><br /><br/><br />
+        &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;
+        <asp:Image ID="Image1" runat="server" Height="100px" Width="100px" /><br /><br /><br />
+         <asp:Button ID="Button1" runat="server" Text="Cost" Height="35px" Width="50px" OnClick="Button1_Click" />
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<asp:Label ID="Label1" runat="server" Text="Cost of the Item"></asp:Label>
+             &nbsp;&nbsp;&nbsp;<asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+    </form>
+</body>
+</html>
---
+++ DropDownlist.aspx.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Asp_DropDown
+{
+    public partial class DropDownlist : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!Page.IsPostBack)
+            {
+
+                string[] str = new string[] { "Select", "IQOOZ3", "AppleIPhone", "OnePlus5", "OppoA74" };
+                for (int i = 0; i < str.Length; i++)
+                {
+                    DropDownList1.Items.Add(str[i]);
+                }
+            }
+
+        }
+
+        protected void DropDownList1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string str = DropDownList1.Text;
+            Image1.ImageUrl = "~/images/" + str + ".jpg";
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            if (DropDownList1.SelectedValue == "IQOOZ3")
+            {
+                TextBox1.Text = "17990";
+            }
+            else if (DropDownList1.SelectedValue == "AppleIPhone")
+            {
+                TextBox1.Text = "30000";
+            }
+            else if (DropDownList1.SelectedValue == "OnePlus5")
+            {
+                TextBox1.Text = "10,990";
+            }
+            else if (DropDownList1.SelectedValue == "OppoA74")
+            {
+                TextBox1.Text = "18000";
+            }
+        }
+    }
+}
