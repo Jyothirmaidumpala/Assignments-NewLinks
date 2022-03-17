3f94fe25f5268bcc534007e1f5f18d281bad487f
Jyothirmaidumpala <98098605+Jyothirmaidumpala@users.noreply.github.com>
2022-03-17 23:20:07 +0530

    MovieTable Sql

---
+++ MovieSql.sql
@@ -0,0 +1,16 @@
+use Northwind
+select*from Customers
+select*from Orders
+
+create database Moviedb
+use Moviedb
+
+create table Movietable(Mid int primary key,Moviename varchar(20),DateofRelease varchar(20))
+insert into Movietable values
+(1,'RadheShyam','11-03-2022'), 
+(2,'Beast','12-02-2023'),
+(3,'Uppena','12-02-2021'),
+(4,'Love story','24-08-2021'),
+(5,'Pushpa','17-12-2021')
+  
+select*from Movietable
